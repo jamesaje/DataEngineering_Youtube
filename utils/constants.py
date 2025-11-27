31894a695d33ee8f1ae2e2961dff9b59d7c8e060
jamesaje <88895237+jamesaje@users.noreply.github.com>
2025-11-27 14:11:04 +0000

    edit constants to remove reddit keys
    Removed Reddit API keys from constants.

--- utils/constants.py
+++ utils/constants.py
@@ -4,8 +4,6 @@ import os
 parser = configparser.ConfigParser()
 parser.read(os.path.join(os.path.dirname(__file__), '../config/config.conf'))
 
-SECRET = parser.get('api_keys', 'reddit_secret_key')
-CLIENT_ID = parser.get('api_keys', 'reddit_client_id')
 YOUTUBE_API_KEY = parser.get('api_keys', 'youtube_api_key')
 
 DATABASE_HOST = parser.get('database', 'database_host')
